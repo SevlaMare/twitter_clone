6333e8068d740f7d358b1a339b26c325876390d8
SevlaMare <sevilamare@gmail.com>
2020-05-18 14:17:24 -0300

    switch to postgres

--- Gemfile
+++ Gemfile
@@ -4,7 +4,6 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 # core
 ruby '~> 2.6'
 gem 'rails', '~> 5.2'
-gem 'sqlite3'
 gem 'puma', '~> 3.11'
 
 # generators
@@ -25,18 +24,23 @@ gem 'bootsnap', '>= 1.1.0', require: false
 
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
-  # gem 'capybara', '>= 2.15'
   gem 'selenium-webdriver'
+  # gem 'capybara', '>= 2.15'
   # gem 'chromedriver-helper'
+  # gem 'rspec-rails', ">= 3.9.0"
 end
 
 group :development do
+  gem 'sqlite3'
   gem 'rubocop'
-  gem 'rspec-rails', ">= 3.9.0"
   gem 'web-console', '>= 3.3.0'
   gem 'listen', '>= 3.0.5', '< 3.2'
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
+end
+
+group :production do
+  gem 'pg'
 end
 
 # Windows compatible
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_05_17_191636) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "tweets", force: :cascade do |t|
     t.text "tweet"
     t.datetime "created_at", null: false
@@ -25,6 +28,11 @@ ActiveRecord::Schema.define(version: 2020_05_17_191636) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
